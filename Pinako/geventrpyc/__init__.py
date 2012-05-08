00e23c37a3f50eec529f4ec5e7d0cb7fcc1f3f31
Matthias Urlichs <matthias@urlichs.de>
2012-05-08 04:48:10 +0200

    Eliminate busy waiting and handle client+server in the same process
    This requires a separate greenlet per dispatch, which probably does
    lots of good things but does NOT speed up RPyC. Sorry.

--- Pinako/geventrpyc/__init__.py
+++ Pinako/geventrpyc/__init__.py
@@ -25,15 +25,23 @@
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ## THE SOFTWARE.
 
+# Extended by Matthias Urlichs <matthias@urlichs.de> to eliminate busy waiting,
+# and to handle client and server running in the same process. This requires
+# a separate greenlet per dispatch, which doesn't exactly speed up RPyC. Sorry.
+
+from __future__ import absolute_import
+
 import sys, logging
-from gevent import socket, spawn
+from gevent import socket, spawn, event, select
 try:
 	from gevent import ssl
 except ImportError:
 	pass
 
-from rpyc.core import VoidService
+_rpyc = __import__('rpyc')
+from rpyc.core import VoidService,Connection,Channel, consts, brine, vinegar, netref
 from rpyc.core.stream import SocketStream as BaseSocketStream
+from rpyc.lib.compat import next, is_py3k, select_error
 from rpyc.utils.server import Server
 from rpyc.utils.factory import connect_stream, _get_free_port
 from rpyc.utils.registry import UDPRegistryClient
@@ -66,13 +74,16 @@ class TunneledSocketStream(SocketStream):
 			self.tun.close()
 
 class GeventServer(Server):
-	def __init__(self,service,hostname='',ipv6=False,port=0,backlog=10,reuse_addr=True,authenticator=None,registrar=None,auto_register=False,protocol_config={},logger=None,listener=None):
+	def __init__(self,service,hostname='',ipv6=False,port=0,backlog=10,reuse_addr=True,authenticator=None,registrar=None,auto_register=None,protocol_config={},logger=None,listener=None):
 		self.active = False
 		self._closed = False
 		self.service = service
 		self.authenticator = authenticator
 		self.backlog = backlog
-		self.auto_register = bool(registrar)
+		if auto_register is None:
+			self.auto_register = bool(registrar)
+		else:
+			self.auto_register = auto_register
 		self.protocol_config = protocol_config
 		self.clients = set()
 		if listener is None:
@@ -87,24 +98,48 @@ class GeventServer(Server):
 			self.listener.bind((hostname,port))
 		else:
 			self.listener = listener
-		self.sockname = self.listener.getsockname()
+		sockname = self.listener.getsockname()
+		self.host, self.port = sockname[0], sockname[1]
 		if logger is None:
-			if isinstance(self.sockname,tuple):
-				logger = logging.getLogger('%s:%d'%(self.service.get_service_name(),self.sockname[1]))
-			else:
-				logger = logging.getLogger('%s:%d'%(self.service.get_service_name(),self.sockname))
+			logger = logging.getLogger("%s/%d" % (self.service.get_service_name(), self.port))
 		self.logger = logger
+		if "logger" not in self.protocol_config:
+			self.protocol_config["logger"] = self.logger
 		if registrar is None:
 			registrar = UDPRegistryClient(logger=self.logger)
 		self.registrar = registrar
+
 	def _accept_method(self,sock):
-		t = spawn(self._authenticate_and_serve_client,sock)
+		spawn(self._authenticate_and_serve_client,sock)
+
+	def _serve_client(self, sock, credentials):
+		addrinfo = sock.getpeername()
+		h = addrinfo[0]
+		p = addrinfo[1]
+		if credentials:
+			self.logger.info("welcome [%s]:%s (%r)", h, p, credentials)
+		else:
+			self.logger.info("welcome [%s]:%s", h, p)
+		try:
+			config = dict(self.protocol_config, credentials = credentials,
+				endpoints = (sock.getsockname(), addrinfo))
+			conn = GeventConnection(self.service, Channel(SocketStream(sock)),
+				config = config, _lazy = True)
+			conn._init_service()
+			## conn.serve_all() ## _init_service() already does that
+			## however, now we need to wait for it
+			## this is a hack
+			q = event.AsyncResult()
+			def done(_):
+				q.set(None)
+			conn._job.link(done)
+			q.get()
+		finally:
+			self.logger.info("goodbye [%s]:%s", h, p)
+
 	def start(self):
 		self.listener.listen(self.backlog)
-		if isinstance(self.sockname,tuple):
-			self.logger.info('server started on [%s]:%s',*self.sockname)
-		else:
-			self.logger.info('server started on %s',self.sockname)
+		self.logger.info("server started on [%s]:%s", self.host, self.port)
 		self.active = True
 		if self.auto_register:
 			spawn(self._bg_register)
@@ -115,6 +150,104 @@ class GeventServer(Server):
 		finally:
 			self.logger.info('server has terminated')
 			self.close()
+
+class GeventConnection(Connection):
+	"""Work around concurrency problems in rpyc"""
+	_job = None
+
+	def _init_service(self):
+		super(GeventConnection,self)._init_service()
+		if self._job is None or self._job.ready():
+			self._job = spawn(self.serve_all)
+
+	def _cleanup(self, _anyway = True):
+		if self._job is not None and not self._job.ready():
+			self._job.kill()
+		self._job = None
+		super(GeventConnection,self)._cleanup(_anyway)
+		
+	def _dispatch_reply(self, seq, raw):
+		"""There are no sync callbacks any more"""
+		obj = self._unbox(raw)
+		self._async_callbacks.pop(seq)(False, obj)
+
+	def _dispatch_exception(self, seq, raw):
+		obj = vinegar.load(raw,
+			import_custom_exceptions = self._config["import_custom_exceptions"],
+			instantiate_custom_exceptions = self._config["instantiate_custom_exceptions"],
+			instantiate_oldstyle_exceptions = self._config["instantiate_oldstyle_exceptions"])
+		self._async_callbacks.pop(seq)(True, obj)
+
+	def _dispatch(self, data):
+		"""Always serve in a separate greenlet. Callbacks can arrive at any time."""
+		spawn(super(GeventConnection,self)._dispatch,data)
+
+	def _send_request(self, handler, args, seq=None):
+		if seq is None:
+			seq = next(self._seqcounter)
+		self._send(consts.MSG_REQUEST, seq, (handler, self._box(args)))
+		return seq
+
+	def _recv(self, timeout, wait_for_lock):
+		if not self._recvlock.acquire(wait_for_lock):
+			return None
+		try:
+			try:
+				if self._channel.poll(timeout):
+					data = self._channel.recv()
+				else:
+					data = None
+			except EOFError:
+				self.close()
+				raise
+		finally:
+			self._recvlock.release()
+		return data
+
+	@staticmethod
+	def _gen_reply(q):
+		def reply(a,b):
+			q.set((a,b))
+		return reply
+
+	def sync_request(self, handler, *args):
+		"""Sends a synchronous request (waits for the reply to arrive)
+		
+		:raises: any exception that the requets may be generated
+		:returns: the result of the request
+		"""
+		seq = next(self._seqcounter)
+		q = event.AsyncResult()
+		self._async_callbacks[seq] = self._gen_reply(q)
+		seq = self._send_request(handler, args, seq=seq)
+		isexc, obj = q.get()
+		if isexc:
+			raise obj
+		else:
+			return obj
+
+	def _async_request(self, handler, args = (), callback = (lambda a, b: None)):
+		seq = next(self._seqcounter)
+		self._async_callbacks[seq] = callback
+		seq = self._send_request(handler, args, seq=seq)
+
+	def serve_all(self):
+		"""Serves all requests and replies for as long as the connection is 
+		alive."""
+		print >>sys.stderr,"START SERVE",self
+		try:
+			while True:
+				self.serve(None)
+		except (socket.error, select_error, IOError):
+			if not self.closed:
+				raise
+		except EOFError:
+			pass
+			
+		finally:
+			print >>sys.stderr,"END SERVE",self
+			self.close()
+
 
 def connect(host,port,service=VoidService,config={},ipv6=False):
 	return connect_stream(SocketStream.connect(host,port,ipv6=ipv6),service,config)
@@ -142,25 +275,33 @@ def ssh_connect(sshctx,remote_port,service=VoidService,config={}):
 	return Connection(service,Channel(stream),config=config)
 
 def patch_select():
-	_select = __import__('gevent.select')
-	_compat = __import__('rpyc.lib.compat')
-	_compat.select = _select
-	_protocol = __import__('rpyc.core.protocol')
-	_protocol.select = _select
+	__import__('rpyc.lib.compat')
+	__import__('rpyc.core.protocol')
+	_rpyc.lib.compat.select = select
+	_rpyc.core.protocol.select = select
+def patch_connection():
+	__import__('rpyc.core')
+	__import__('rpyc.core.protocol')
+	__import__('rpyc.utils.factory')
+	__import__('rpyc.utils.server')
+	_rpyc.core.Connection = GeventConnection
+	_rpyc.core.protocol.Connection = GeventConnection
+	_rpyc.utils.factory.Connection = GeventConnection
+	_rpyc.utils.server.Connection = GeventConnection
 def patch_stream():
-	_stream = __import__('rpyc.core.stream')
-	_stream.SocketStream = SocketStream
-	_stream.TunneledSocketStream = TunneledSocketStream
+	__import__('rpyc.core.stream')
+	_rpyc.core.stream.SocketStream = SocketStream
+	_rpyc.core.stream.TunneledSocketStream = TunneledSocketStream
 def patch_factory():
-	_factory = __import__('rpyc.utils.factory')
-	_factory.connect = connect
-	_factory.tlslite_connect = tlslite_connect
-	_factory.ssl_connect = ssl_connect
-	_factory.ssh_connect = ssh_connect
+	__import__('rpyc.utils.factory')
+	_rpyc.utils.factory.connect = connect
+	_rpyc.utils.factory.tlslite_connect = tlslite_connect
+	_rpyc.utils.factory.ssl_connect = ssl_connect
+	_rpyc.utils.factory.ssh_connect = ssh_connect
 def patch_server():
-	_server = __import__('rpyc.utils.server')
-	_server.ThreadedServer = GeventServer
-def patch_all(select=True,stream=True,factory=True,server=True):
+	__import__('rpyc.utils.server')
+	_rpyc.utils.server.ThreadedServer = GeventServer
+def patch_all(select=True,stream=True,factory=True,server=True,connection=True):
 	if select:
 		patch_select()
 	if stream:
@@ -169,5 +310,7 @@ def patch_all(select=True,stream=True,factory=True,server=True):
 		patch_factory()
 	if server:
 		patch_server()
+	if connection:
+		patch_connection()
 
 __all__ = ['SocketStream','TunneledSocketStream','GeventServer','connect','tlslite_connect','ssl_connect','ssh_connect','patch_select','patch_stream','patch_factory','patch_server','patch_all']
